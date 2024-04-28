2773c93cdbb2b5571b09f61c130baee3ae1ebc25
Filippo Varini <filippo@alpha10x.com>
2024-04-28 14:46:39 -0700

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Filippo Varini',
+			social: {
+				github: 'https://github.com/filippovarini',
+				linkedin: 'https://www.linkedin.com/in/filippo-varini/'
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
