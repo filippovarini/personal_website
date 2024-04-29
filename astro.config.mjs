89f35ead8b8f2784e09b077e52d56c5864b66d02
Filippo Varini <filippo@alpha10x.com>
2024-04-29 08:25:14 -0700

    add logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,12 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Filippo Varini',
+			customCss: [
+				'./src/styles/custom.css',
+			  ],
+			logo: {
+				src: './src/assets/cactus.svg',
+			  },
 			social: {
 				github: 'https://github.com/filippovarini',
 				linkedin: 'https://www.linkedin.com/in/filippo-varini/'
